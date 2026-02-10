c1794a910bd966eec0fd4c9206e262ce1add2886
ayushisharmaprofile <ayushisharma.profile@gmail.com>
2026-02-09 21:02:39 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="medical_rag", 
+    version="0.1.0",
+    author="AS",
+    author_email="ayushisharma.profile@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
