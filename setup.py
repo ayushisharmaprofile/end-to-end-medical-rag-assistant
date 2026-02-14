43a3ded599815718d510b8f3ac5e52d39b825e22
Ayushi Sharma <155859186+ayushisharmaprofile@users.noreply.github.com>
2026-02-14 18:40:26 -0500

    working code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="medical_rag", 
     version="0.1.0",
     author="AS",
-    author_email="ayushisharma.profile@gmail.com",
+    author_email="xyz@gmail.com",
     packages=find_packages(),
     install_requires=[
         "langchain==0.3.0",
